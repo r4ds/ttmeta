f09f9e6ad76fb82ce1431da09a9674057dad723e
Jon Harmon <jonthegeek@gmail.com>
2023-05-24 10:40:16 -0500

    Update datasets with GHA.
    Unfortunately I can't know if this ENTIRELY works until I run it on main, so I'll have to merge semi-blind.

---
+++ data-raw/just-new.R
@@ -0,0 +1,45 @@
+pkgload::load_all()
+
+updated_tt_summary_tbl <- get_tt_tbl()
+new_datasets <- dplyr::filter(
+  updated_tt_summary_tbl,
+  !(date %in% tt_summary_tbl$date)
+)
+
+new_tt_datasets_metadata <- get_tt_datasets_metadata(new_datasets)
+updated_tt_datasets_metadata <- tt_datasets_metadata |>
+  # Make sure the new stuff isn't already there.
+  dplyr::anti_join(
+    new_tt_datasets_metadata,
+    by = dplyr::join_by(year, week)
+  ) |>
+  dplyr::bind_rows(new_tt_datasets_metadata) |>
+  dplyr::arrange(year, week, dataset_name)
+
+if (
+  !identical(updated_tt_summary_tbl, tt_summary_tbl) ||
+  !identical(updated_tt_datasets_metadata, tt_datasets_metadata)
+) {
+  tt_summary_tbl <- updated_tt_summary_tbl
+  tt_datasets_metadata <- updated_tt_datasets_metadata
+  .tt_gh_base <- "https://github.com/rfordatascience/tidytuesday/blob/master/"
+  usethis::use_data(
+    .tt_gh_base,
+    tt_summary_tbl,
+    tt_datasets_metadata,
+    overwrite = TRUE,
+    internal = TRUE
+  )
+  rm(
+    .tt_gh_base,
+    tt_summary_tbl,
+    tt_datasets_metadata
+  )
+}
+
+rm(
+  new_datasets,
+  new_tt_datasets_metadata,
+  updated_tt_summary_tbl,
+  updated_tt_datasets_metadata
+)
