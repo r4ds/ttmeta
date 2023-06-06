2426e686265bde2d5958c62ca0c4e72b61314f4c
Jon Harmon <jonthegeek@gmail.com>
2023-06-05 21:30:56 -0500

    Fix date errors.

--- R/summary-tbl.R
+++ R/summary-tbl.R
@@ -68,16 +68,28 @@ get_tt_tbl <- function(min_year = 2018L,
 .parse_tt_year_readme <- function(year) {
   rows <- .tt_year_readme_rows(year)
 
-  return(
-    dplyr::tibble(
-      year = year,
-      week = as.integer(.extract_text_col(rows, 1)),
-      date = lubridate::as_date(.extract_text_col(rows, 2)),
-      title = .extract_text_col(rows, 3),
-      source_title = .extract_text_col(rows, 4),
-      source_urls = .extract_urls_col(rows, 4),
-      article_title = .extract_text_col(rows, 5),
-      article_urls = .extract_urls_col(rows, 5)
-    )
+  parsed <- dplyr::tibble(
+    year = year,
+    week = as.integer(.extract_text_col(rows, 1)),
+    date = lubridate::as_date(.extract_text_col(rows, 2)),
+    title = .extract_text_col(rows, 3),
+    source_title = .extract_text_col(rows, 4),
+    source_urls = .extract_urls_col(rows, 4),
+    article_title = .extract_text_col(rows, 5),
+    article_urls = .extract_urls_col(rows, 5)
   )
+
+  # Check data and alert if something is weird.
+  tues_check <- .is_tuesday(parsed$date)
+
+  if (any(!tues_check)) {
+    bad_dates <- parsed$date[!tues_check]
+
+    bad_dates <- glue::glue_collapse(parsed$date[!tues_check], sep = "\n")
+    cli::cli_alert_warning(
+      "These dates are not Tuesday: {bad_dates}"
+    )
+  }
+
+  return(parsed)
 }
